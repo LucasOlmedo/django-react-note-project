553289d2ed472a70e589a912ac500f70c1a30a4f
Lucas Olmedo Silva <lucas.olmedo@doc88.com.br>
2024-06-07 16:34:43 -0300

    initial commit

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "../styles/Note.css"
+
+function Note({ note, onDelete }) {
+    const date = new Date(note.created_at).toLocaleDateString("pt-BR")
+
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{date}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    )
+}
+
+export default Note
